38e1015ba14c76c8ac9cb1326173974d5e145f0f
Ocean <oceanacademypuducherry@gmail.com>
2021-03-22 16:46:28 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/gomathi/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package gomathi.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/result_page.dart
@@ -0,0 +1,75 @@
+import 'bottom_button.dart';
+import 'package:bmi_calculator/constant.dart';
+import 'reusable_card.dart';
+import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  ResultPage(
+      {@required this.resultText,
+      @required this.bmiResult,
+      @required this.interpretation});
+  final String resultText;
+  final String bmiResult;
+  final String interpretation;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your Result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kInactiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    'Normal BMI range: '
+                    '18,5-25 kg/m2',
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    bmiResult,
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation,
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
